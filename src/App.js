11ab9b2fbc3e43ff031d09e1f0989319f3f4ffb8
Denisa Ponce <denisaponce2012@hotmail.com>
2022-02-06 21:04:09 -0300

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from React;
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
+
 function App() {
+const [user, setUser] = useState("");
+const [bienvenido, setBienvenido] = useState("");
+
+useEffect(() => {
+  if (bienvenido){
+    alert(`Gracias por tu vista 👧`)
+  }
+}, [bienvenido]);
+
   return (
-    
+    <div className="container">
+       <h1>Hello User App</h1>
+       <form className='Formulario' >
+       <input type="text" placeholder='Ingresa tu Nombre' value={user} onChange={(e) => setUser(e.target.value)}/>
+       <button type='button' onClick={() =>setBienvenido(`¡Bienvenido/a ${user}! 😄`)}>¡Saludame!</button>
+       </form>
+       <p>{bienvenido ? bienvenido : "Ingresa tu nombre 🧐"}</p>
+    </div>
   );
 }
-
 export default App;
